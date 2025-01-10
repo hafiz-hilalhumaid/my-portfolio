0fb20bccd828f7f0b6b60c067fb1aeabf1d33527
HH <hafizhilalhumaid@gmail.com>
2025-01-10 12:03:04 +0530

    Portfolio

---
+++ src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "Hafiz";
+const meta = {
+  title: "Hafiz Hilal Humaid",
+  description:
+    "I’m Hafiz Hilal Humaid _ Full stack devloper,currently working in Bengaluru, India.",
+};
+
+const introdata = {
+  title: "I’m Hafiz Hilal Humaid",
+  animated: {
+    first: "I love coding",
+    second: "Expertise: MERN",
+    third: "I develop Web apps",
+  },
+  description:
+    "Passionate MERN Stack Developer with experience in building dynamic and responsive web applications. Proficient in React, Redux, express, MongoDB and Node.js. Adept at collaborating with cross-functional teams to deliver high-quality software solutions.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Passionate MERN Stack Developer with experience in building dynamic and responsive web applications. Proficient in React, Redux, express, MongoDB and Node.js. Adept at collaborating with cross-functional teams to deliver high-quality software solutions.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Freelanced",
+    where: "Remote",
+    date: "Sep 2021 - Dec 2021",
+  },
+  {
+    jobtitle: "MERN Stack Developer",
+    where: "Bengaluru, India",
+    date: "Jan 2022 - Present",
+  },
+];
+
+const skills = [
+  {
+    name: "React js",
+    value: 90,
+  },
+  {
+    name: "Redux",
+    value: 85,
+  },
+  {
+    name: "React Hooks",
+    value: 85,
+  },
+  {
+    name: "Material UI",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Typescript",
+    value: 60,
+  },
+  {
+    name: "Node.js",
+    value: 85,
+  },
+  {
+    name: "Express",
+    value: 85,
+  },
+  {
+    name: "MongoDB",
+    value: 85,
+  },
+  {
+    name: "Mongoose ODM",
+    value: 85,
+  },
+];
+const tools = [
+  {
+    name: "GitHub",
+    value: 90,
+  },
+  {
+    name: "Jira",
+    value: 85,
+  },
+  {
+    name: "Postman",
+    value: 85,
+  },
+];
+const services = [
+  {
+    title: "Web Dev",
+    description:
+      "I develope full stack web applications uisng React.js, Redux, Node.js, Express and MongoDB",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "MRI-Simmons and Research Applicaton",
+    link: "https://www.mrisimmons.com/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Yearbook Application",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "hafizhilalhumaid@gmail.com",
+  YOUR_FONE: "(+91) 8667279608",
+  description:
+    "    Passionate MERN Stack Developer with experience in building dynamic and responsive web applications. Proficient in React, Redux, express, MongoDB and Node.js..",
+};
+
+const socialprofils = {
+  linkedin: "www.linkedin.com/in/hafiz-hilal-humaid-63a926344",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+  tools,
+};
